8102dcb0cfaae97f6813c0fc317c949f10ae1efe
RAVVA2799 <80970669+RAVVA2799@users.noreply.github.com>
2022-09-02 11:54:24 +0530

    Add files via upload

---
+++ es6letconst.js
@@ -0,0 +1,22 @@
+let  x = 10;
+// Here x is 10
+
+{  
+  let x = 2;
+  // Here x is 2
+}
+
+// Here x is 10
+console.log(x);
+
+
+let  y = 150;
+// Here y is 10
+
+{  
+  let y = 2;
+  // Here y is 2
+}
+
+// Here y is 10
+console.log(y);
